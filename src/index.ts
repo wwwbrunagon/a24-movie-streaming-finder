20f758c17a0a7101cd91a4f004d054532380ef59
wwwbrunagon <wwwbrunagon@gmail.com>
2024-11-23 11:37:53 -0300

    updating logger info for start server

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import express from 'express';
 import studioRoutes from './routes/studioRoutes';
 import { errorHandlerMiddleware } from './middleware/errorHandlerMiddleware';
+import Logger from './utils/logger';
 
 const app = express();
 const PORT = 3000;
 
 app.use('/api', studioRoutes);
-
 app.use(errorHandlerMiddleware);
-
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  Logger.info(`Server is running on http://localhost:${PORT}`); // Using Logger here
 });
