d436b4ccc46d2cca14317e8b2964ecc8d6072d7e
wwwbrunagon <wwwbrunagon@gmail.com>
2024-11-24 15:56:52 -0300

    set up ok

--- backend/src/services/companyService.ts
+++ backend/src/services/companyService.ts
@@ -1,16 +1,20 @@
 import { Company } from '../interfaces/company';
-import companiesDataRaw from '../../backend/data/production_company_ids_11_22_2024.json';
+import companiesDataRaw from '../../data/production_company_ids_11_22_2024.json';
 
 const companiesData: Company[] = (companiesDataRaw as { data: Company[] }).data;
 
 // Function to get a company by ID
 export const getCompanyById = async (id: number): Promise<Company | null> => {
-  const company = companiesData.find((company) => company.id === id);
-  return company || null;
+	const company = companiesData.find((company) => company.id === id);
+	return company || null;
 };
 
 // Function to get a company by name
-export const getCompanyByName = async (name: string): Promise<Company | null> => {
-  const company = companiesData.find((company) => company.name.toLowerCase() === name.toLowerCase());
-  return company || null;
+export const getCompanyByName = async (
+	name: string
+): Promise<Company | null> => {
+	const company = companiesData.find(
+		(company) => company.name.toLowerCase() === name.toLowerCase()
+	);
+	return company || null;
 };
